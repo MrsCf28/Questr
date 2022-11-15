0cbe249e7e666b3b817bcfdf3d657e649b03bb70
jonny-barker <jonny.d.barker@gmail.com>
2022-11-15 12:31:34 +0000

    added edit profile form and setting up the user context

--- native-app/components/ProfileController.tsx
+++ native-app/components/ProfileController.tsx
@@ -1,41 +1,45 @@
-import React from 'react'
-import {View, Pressable, StyleSheet, Text} from 'react-native'
-
+import React from "react";
+import { View, Pressable, StyleSheet, Text } from "react-native";
+import {
+  useNavigation,
+  usePreventRemoveContext,
+} from "@react-navigation/native";
 
 export function ProfileController() {
-
-    return (
-        <View style={styles.container}>
-            <Pressable style={styles.button}>
-                <Text style={styles.text}> Sign out</Text>
-            </Pressable>
-            <Pressable style={styles.button}>
-                <Text style={styles.text}>Edit Profile</Text>
-            </Pressable>
-        </View>
-    )
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <Pressable style={styles.button}>
+        <Text style={styles.text}> Sign out</Text>
+      </Pressable>
+      <Pressable
+        style={styles.button}
+        onPress={() => navigation.navigate("EditProfile")}>
+        <Text style={styles.text}>Edit Profile</Text>
+      </Pressable>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      flexDirection: 'row',
-      padding: 20,
-    },
-    button: {
-        margin: 10,
-        backgroundColor: 'blue',
-        padding: 10,
-        color: 'white',
-        borderRadius: 20,
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    text: {
-        color: 'white'
-    }
-  });
-  
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    padding: 20,
+  },
+  button: {
+    margin: 10,
+    backgroundColor: "blue",
+    padding: 10,
+    color: "white",
+    borderRadius: 20,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "white",
+  },
+});
