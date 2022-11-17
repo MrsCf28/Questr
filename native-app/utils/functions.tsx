b56a5c3e15b3dd7619585b63094d9df69a4a88a0
KonaroKam <qamar-usman@hotmail.com>
2022-11-17 10:26:33 +0000

    add missingWord game core code. Tested and working in app. No input method yet

--- native-app/utils/functions.tsx
+++ native-app/utils/functions.tsx
@@ -1,14 +1,26 @@
-
-export const locationChecker = (questLocation, myLocation, accuracy) => {
-    console.log(myLocation, questLocation)
-    const me = {lat: myLocation.latitude.toFixed(accuracy), long: myLocation.longitude.toFixed(accuracy)}
-    const quest = {lat: questLocation.latitude.toFixed(accuracy), long: questLocation.longitude.toFixed(accuracy)}
-    if(me.lat === quest.lat && me.long === quest.long) {
-        return 'true'
-    } else {
-        return 'false'
-    }
+type location = {
+	longitude: number,
+	latitude: number,
+	latitudeDelta: number,
+    longitudeDelta: number,
 }
+
+export const locationChecker = (questLocation: location, myLocation: location, accuracy: number) => {
+	console.log(myLocation, questLocation);
+	const me = {
+		lat: myLocation.latitude.toFixed(accuracy),
+		long: myLocation.longitude.toFixed(accuracy),
+	};
+	const quest = {
+		lat: questLocation.latitude.toFixed(accuracy),
+		long: questLocation.longitude.toFixed(accuracy),
+	};
+	if (me.lat === quest.lat && me.long === quest.long) {
+		return "true";
+	} else {
+		return "false";
+	}
+};
 
 export function formatUserStats({
 	dexterity,
@@ -25,27 +37,41 @@ export function formatUserStats({
 	exploration: number;
 	strength: number;
 }) {
-
-	const maxStatOnRadarChart = (Math.max.apply(
-		Math,
-		Object.values({
-			dexterity,
-			exploration,
-			perception,
-			stamina,
-			strength,
-			wisdom,
-		})
-	))*0.9;
+	const maxStatOnRadarChart =
+		Math.max.apply(
+			Math,
+			Object.values({
+				dexterity,
+				exploration,
+				perception,
+				stamina,
+				strength,
+				wisdom,
+			})
+		) * 0.9;
 
 	const formattedStats: number[] = [
-		dexterity/maxStatOnRadarChart,
-		exploration/maxStatOnRadarChart,
-		perception/maxStatOnRadarChart,
-		stamina/maxStatOnRadarChart,
-		strength/maxStatOnRadarChart,
-		wisdom/maxStatOnRadarChart,
+		dexterity / maxStatOnRadarChart,
+		exploration / maxStatOnRadarChart,
+		perception / maxStatOnRadarChart,
+		stamina / maxStatOnRadarChart,
+		strength / maxStatOnRadarChart,
+		wisdom / maxStatOnRadarChart,
 	];
 
 	return formattedStats;
 }
+
+
+export function missingWordGame(
+	requiredWords: string[],
+	guessedWords: string[]
+) {
+	for (let i = 0; i < requiredWords.length; i++) {
+		if (guessedWords[i].toLowerCase() !== requiredWords[i].toLowerCase()) {
+			return false;
+		}
+	}
+	return true;
+}
+
