b157f45c9fce500d2dbd522205b997952d6f1f6f
MrsCf28 <rebeccaflynn28@outlook.com>
2022-11-17 01:23:02 +0000

    signUp context

--- native-app/App.tsx
+++ native-app/App.tsx
@@ -1,72 +1,77 @@
-import React, { useState, useEffect } from "react";
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Amplify, API, graphqlOperation, Auth } from "aws-amplify";
-import { withAuthenticator } from "aws-amplify-react-native";
+import React, { useState, useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Amplify, API, graphqlOperation, Auth } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react-native';
 
-import useCachedResources from "./hooks/useCachedResources";
-import useColorScheme from "./hooks/useColorScheme";
-import Navigation from "./navigation";
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
 
-import { CurrentUser } from "./context/CurrentUser";
-import { getQuestApi, listQuestApis } from "./src/graphql/queries";
+import { CurrentUser } from './context/CurrentUser';
+import { SignedUp } from './context/SignedUp';
 
-import awsExports from "./src/aws-exports";
-import { fetchAllQuests, fetchQuestById } from "./utils/questApi";
-import { addUser, fetchUserById, fetchUserIdsPlay } from "./utils/userApi";
-import EditProfileScreen from "./screens/EditProfileScreen";
+import awsExports from './src/aws-exports';
+import {
+    fetchUserById,
+
+} from './utils/userApi';
+import EditProfileScreen from './screens/EditProfileScreen';
 
 Amplify.configure({
-  ...awsExports,
-  Analytics: {
-    disabled: true,
-  },
+    ...awsExports,
+    Analytics: {
+        disabled: true,
+    },
 });
 
 function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+    const isLoadingComplete = useCachedResources();
+    const colorScheme = useColorScheme();
 
-  const [currentUser, setCurrentUser] = useState({
-    image: "https://picsum.photos/200/300",
-  });
-  // const [allQuests, setAllQuests] = useState([]);
-  const [id, setId] = useState(Auth.user.attributes.sub);
-  const [userId, setUserId] = useState("4");
-  const [newUser, setNewUser] = useState({ id: "24", age: 22 });
-  const [signedUp, setSignedUp] = useState(true);
-
-  useEffect(() => {
-    fetchUserById(id).then((user) => {
-      if (user === null) {
-        setSignedUp(false);
-        setCurrentUser({ ...currentUser, id: id });
-      } else {
-        setSignedUp(true);
-        setCurrentUser(user);
-      }
+    const [currentUser, setCurrentUser] = useState({
+        image: 'https://picsum.photos/200/300',
     });
-  }, []);
+    // const [allQuests, setAllQuests] = useState([]);
+    const [id, setId] = useState(Auth.user.attributes.sub);
+    const [userId, setUserId] = useState('4');
+    const [newUser, setNewUser] = useState({ id: '24', age: 22 });
+    const [signedUp, setSignedUp] = useState(true);
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
-          {signedUp ? (
-            <Navigation colorScheme={colorScheme} />
-          ) : (
-            <EditProfileScreen
-              signedUp={signedUp}
-              setSignedUp={setSignedUp}
-            />
-          )}
-          <StatusBar />
-        </CurrentUser.Provider>
-      </SafeAreaProvider>
-    );
-  }
+    useEffect(() => {
+        fetchUserById(id).then(user => {
+            if (user === null) {
+                setSignedUp(false);
+                setCurrentUser({ ...currentUser, id: id });
+            } else {
+                setSignedUp(true);
+                setCurrentUser(user);
+            }
+        });
+    }, []);
+
+    if (!isLoadingComplete) {
+        return null;
+    } else {
+        return (
+            <SafeAreaProvider>
+                <CurrentUser.Provider
+                    value={{ currentUser, setCurrentUser }}
+                >
+                  <SignedUp.Provider
+                  value={{ signedUp, setSignedUp }}
+                  >
+                    {signedUp ? (
+                        <Navigation colorScheme={colorScheme} />
+                    ) : (
+                        <EditProfileScreen />
+                    )}
+                    <StatusBar />
+                    </SignedUp.Provider>
+                </CurrentUser.Provider>
+            </SafeAreaProvider>
+        );
+    }
 }
 
 export default withAuthenticator(App);
