f6321dc7d4e20da9721d522ca8e50a3b940ef17f
AaronCurrie <93119982+AaronCurrie@users.noreply.github.com>
2022-11-18 17:50:32 +0000

    cahnges to avatar shop

--- native-app/screens/AvatarSelector.tsx
+++ native-app/screens/AvatarSelector.tsx
@@ -22,17 +22,17 @@ const Elf = require('../assets/images/elfknight.png')
 export function AvatarSelector() {
 
 
-    
+    //This page could do with a refactor!! change alot of the buttons and conditional logic in to sepereate components
+
     const navigation = useNavigation();
 
     const {setCurrentUser, currentUser} = useContext(CurrentUser)
     const [avatarArray, setAvatarArray] = useState([{id:0, image: Bard, cost: 100}, {id:1, image: Jester, cost: 200}, {id:2, image: BlackSmith, cost: 300}, {id:3, image: Knight, cost: 400}, {id:4, image: ManAtArms, cost: 500}, {id:5, image: Mage, cost: 600}, {id:6, image: Elf, cost: 700}, {id:7, image: Princess, cost: 800}, {id:8, image: King, cost: 900}, {id:9, image: DeathKnight, cost: 1000}])
     const [currentAvatar, setCurrentAvatar] = useState(0)
-
-    //can this array exsit on the user??
+    const [popup, setPopup] = useState(false)
 
     const {coins} = currentUser.stats
-    // working on this to check if avatar exists in bought array
+
     function avatarChecker() {
       if(currentUser.owned_avatar_ids.some((avatar) => avatar === avatarArray[currentAvatar].id)) {
         return true
@@ -58,34 +58,46 @@ export function AvatarSelector() {
     }
 
     function select() {
-        
       setCurrentUser({...currentUser, avatar_uri: `${currentAvatar}` })
 
       const updatedUser = {
         id: currentUser.id,
-        display_name: currentUser.display_name,
         age: currentUser.age,
-        preferred_region: currentUser.preferred_region,
-        image: currentUser.image,
         current_quest_id: currentUser.current_quest_id,
-        quest_history: currentUser.quest_history,
         avatar_uri: `${currentAvatar}`,
-        stats: currentUser.stats
       };
       patchUser(updatedUser).then(() => {
         console.log('patched')
-      }).catch((err: any) => {
-        
+      }).catch((err: any) => {    
         console.log("error in patch user", err);
       });
         navigation.goBack()
     }
 
-
     function buy() {
-
       const newBoughtAvatars = [...currentUser.owned_avatar_ids, avatarArray[currentAvatar].id]
       const newStats = {...currentUser.stats, coins: currentUser.stats.coins - avatarArray[currentAvatar].cost}
+      setCurrentUser({...currentUser, owned_avatar_ids: newBoughtAvatars, stats: newStats })
+
+      const updatedUser = {
+        id: currentUser.id,
+        age: currentUser.age,
+        current_quest_id: currentUser.current_quest_id,
+        stats: newStats,
+        owned_avatar_ids: newBoughtAvatars
+      };
+
+      patchUser(updatedUser).then(() => {
+        console.log('patched')
+      }).catch((err: any) => {
+        console.log("error in patch user", err);
+      });
+    }
+
+    function sell() {
+      if(currentUser.owned_avatar_ids.length > 1 && Number(currentUser.avatar_uri) !== currentAvatar) {
+      const newBoughtAvatars = currentUser.owned_avatar_ids.filter(avatar => avatar !== currentAvatar)
+      const newStats = {...currentUser.stats, coins: currentUser.stats.coins + 50}
 
       setCurrentUser({...currentUser, owned_avatar_ids: newBoughtAvatars, stats: newStats })
 
@@ -100,10 +112,15 @@ export function AvatarSelector() {
       patchUser(updatedUser).then(() => {
         console.log('patched')
       }).catch((err: any) => {
-        
         console.log("error in patch user", err);
       });
+    } else {
+      setPopup(true)
+      setTimeout(() => {
+        setPopup(false)
+      }, 1000)
     }
+  }
 
   return (
     <View
@@ -118,12 +135,15 @@ export function AvatarSelector() {
             </View>
             <View style={{flexDirection: 'row', width: '90%'}}>
               <View style={styles.plaque}>
-                  {!avatarChecker()? <Text style={styles.text}>{avatarArray[currentAvatar].cost} Gold</Text> : <Text style={styles.text}>Owned</Text>}
+                  {!avatarChecker()? <Text style={styles.text}>{avatarArray[currentAvatar].cost} Gold</Text> : <Text style={styles.text}>{Number(currentUser.avatar_uri) !== currentAvatar?'Owned':'Selected'}</Text>}
               </View>
-              <Pressable disabled={avatarChecker() || coins < avatarArray[currentAvatar].cost} style={[styles.button, avatarChecker() || coins < avatarArray[currentAvatar].cost? styles.disabledButton : null]} onPress={buy}>
-                {/* will disable and turn red if owned */}
-                  <Text style={styles.text}>Buy</Text>
-              </Pressable>
+              { !avatarChecker()?
+              <Pressable style={[styles.button, coins < avatarArray[currentAvatar].cost? styles.disabledButton : null]} onPress={buy}>
+                  <Text style={styles.text}>{coins < avatarArray[currentAvatar].cost? 'Insufficent Funds' : 'Buy'}</Text>
+              </Pressable> :
+              <Pressable style={[styles.button, avatarChecker()? styles.disabledButton : null]} onPress={sell}>
+                  <Text style={styles.text}>Sell: 50 Gold</Text>
+              </Pressable> }
             </View>
 
             <View style={[styles.holder, {flex:1, flexDirection: 'row'}]}>
@@ -135,11 +155,15 @@ export function AvatarSelector() {
                 </Pressable>
             </View>
             <View style={styles.selectButton}>
-              {/* will enable and turn green if owned */}
+              {avatarChecker() && Number(currentUser.avatar_uri) !== currentAvatar?
                 <Pressable disabled={!avatarChecker()} style={[styles.button, {width: '50%'}, avatarChecker()? null : styles.disabledButton]} onPress={select}>
                     <Text style={styles.text}>Select</Text>
-                </Pressable>
+                </Pressable> : null}
             </View>
+            {popup?
+            <View style={styles.overlay}>
+              <Text style={styles.text}>{Number(currentUser.avatar_uri) !== currentAvatar?'Must Have At Least One Avatar' : 'Cant Sell Current Avatar'}</Text>
+            </View> : null}
         </ImageBackground>
     </View>
   );
@@ -212,9 +236,25 @@ const styles = StyleSheet.create({
         borderColor: '#d4d4d4',
         backgroundColor: '#292936',
         borderRadius: 20,
-        padding: 5,
+        width: '50%'
     },
     disabledButton: {
       backgroundColor: '#4a040c'
+    },
+
+    overlay: {
+      position: 'absolute',
+      bottom: '30%',
+      margin: 10,
+      padding: 10,
+      color: "white",
+      borderRadius: 20,
+      width: '100%',
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      borderWidth: 5,
+      borderColor: '#7a7877',
+      backgroundColor: '#4a040c',
     }
   });
