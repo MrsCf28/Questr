b646ac29955df00f892554aec528f482a60a8be2
MrsCf28 <rebeccaflynn28@outlook.com>
2022-11-22 14:14:49 +0000

    started imgurAPi testing

---
+++ native-app/utils/_utilsTests_/imgurApi.test.tsx
@@ -0,0 +1,8 @@
+import { postImage } from "../imgurApi";
+// import ghastly from './Ghastly3.png'
+
+describe('imgurApi tests', () => {
+    test('returns a string', () => {
+        // const imageToUpload = ghastly
+    })
+})
