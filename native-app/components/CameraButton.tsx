ba8a75bef6f6523e32def81e81230677c6e56745
Alex Smith <109144005+AlexMSm@users.noreply.github.com>
2022-11-22 10:23:27 +0000

    imgur image url integration added

--- native-app/components/CameraButton.tsx
+++ native-app/components/CameraButton.tsx
@@ -1,47 +1,47 @@
-import * as React from 'react';
-import { TouchableOpacity, StyleSheet } from 'react-native';
-import { Entypo } from '@expo/vector-icons';
+import * as React from "react";
+import { TouchableOpacity, StyleSheet } from "react-native";
+import { Entypo } from "@expo/vector-icons";
 
 type CameraButtonProps = {
-    title: string;
-    onPress: () => Promise<void>;
-    icon: string;
-    color: string;
+  title: string;
+  onPress: () => Promise<void>;
+  icon: string;
+  color: string;
 };
 
 export default function CameraButton({
-    title,
-    onPress,
-    icon,
-    color,
+  title,
+  onPress,
+  icon,
+  color,
 }: CameraButtonProps) {
-    return (
-        <TouchableOpacity onPress={onPress} disabled={true} style={StyleSheet.button}>
-            <Entypo
-                name={icon}
-                size={28}
-                color={color ? color : '#f1f1f1'}
-                style={styles.icon}
-            />
-        </TouchableOpacity>
-    );
+  return (
+    <TouchableOpacity onPress={onPress} style={StyleSheet.button}>
+      <Entypo
+        name={icon}
+        size={28}
+        color={color ? color : "#f1f1f1"}
+        style={styles.icon}
+      />
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-    button: {
-        height: 40,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        margin: 20,
-    },
-    text: {
-        fontWeight: 'bold',
-        fontSize: 16,
-        color: '#f1f1f1',
-        marginLeft: 10,
-    },
-    icon: {
-        margin: 40,
-    },
+  button: {
+    height: 40,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 20,
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "#f1f1f1",
+    marginLeft: 10,
+  },
+  icon: {
+    margin: 40,
+  },
 });
